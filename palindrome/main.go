8125ddd20544fbf6abbd1cd519c9e7421354cff4
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-24 00:31:16 -0700

    palindrome code clean up

--- palindrome/main.go
+++ palindrome/main.go
@@ -8,10 +8,8 @@ func checkPalindrome(str string) bool {
 	for i, j := 0, len(str)-1; i <= j; i, j = i+1, j-1 {
 		if str[i] == str[j] {
 			palindrome = true
-			fmt.Println(str[i], str[j])
 		} else {
 			palindrome = false
-			fmt.Println(str[i], str[j])
 		}
 	}
 	return palindrome

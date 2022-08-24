86ef0a0346b1f3fa83485a98933210760c6a697e
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-24 00:27:36 -0700

    palindrome solution two

--- palindrome/main.go
+++ palindrome/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Solution 1
 func checkPalindrome(str string) bool {
 	var palindrome bool
 	for i, j := 0, len(str)-1; i <= j; i, j = i+1, j-1 {
@@ -16,7 +17,24 @@ func checkPalindrome(str string) bool {
 	return palindrome
 }
 
+// Solution 2
+func checkPalindrome2(str string) bool {
+	var palindrome bool
+	newStr := ""
+	for _, v := range str {
+		newStr = string(v) + newStr
+		if str == newStr {
+			palindrome = true
+		} else {
+			palindrome = false
+		}
+	}
+	return palindrome
+}
+
 func main() {
+	// gevin string
 	str := "racecar"
 	fmt.Println(checkPalindrome(str))
+	fmt.Println(checkPalindrome2(str))
 }
